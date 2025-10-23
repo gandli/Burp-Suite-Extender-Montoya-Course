cb3c3f35f797e176f0e37fb54ad37cf23a672f2a
gandli <gandli@qq.com>
2025-10-23 09:32:00 +0800

    ci: 添加 GitHub Actions 和 Dependabot 配置文件
    添加 Gradle 构建的 CI 工作流和依赖更新配置

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,4 +1,15 @@
+// 定义一个包名，通常按照你的项目或公司规范来命名，这里为 "helloworld"
 package helloworld;
 
-public class HelloWorld {
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.logging.Logging;
+
+public class HelloWorld implements BurpExtension{
+
+    @Override
+    public void initialize(MontoyaApi api) {
+        api.extension().setName("Hello world extension");
+
+    }
 }
